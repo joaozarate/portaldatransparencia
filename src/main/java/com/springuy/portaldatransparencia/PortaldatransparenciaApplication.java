9e51ceba92320fe771c5faadbb8fa67bf540c9e2
Joao Zarate <joaozarate@gmail.com>
2023-09-03 01:18:01 +0100

    Initial commit

---
+++ src/main/java/com/springuy/portaldatransparencia/PortaldatransparenciaApplication.java
@@ -0,0 +1,13 @@
+package com.springuy.portaldatransparencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortaldatransparenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortaldatransparenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springuy/portaldatransparencia/PortaldatransparenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springuy.portaldatransparencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortaldatransparenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
