0f3b84d8fa727a4834de397c714d5d4f925993e0
Joao Zarate <joaozarate@gmail.com>
2023-09-21 22:47:02 +0100

    [TP-1] - This completes the first version of connecting to the third-party API.

--- src/main/java/com/springuy/portaldatransparencia/framework/PortalDaTransparenciaConsumer.java
+++ src/main/java/com/springuy/portaldatransparencia/framework/PortalDaTransparenciaConsumer.java
@@ -2,12 +2,16 @@ package com.springuy.portaldatransparencia.framework;
 
 import lombok.Cleanup;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Level;
 
+@Log
 @RequiredArgsConstructor
 class PortalDaTransparenciaConsumer implements Consumer {
 
@@ -18,9 +22,15 @@ class PortalDaTransparenciaConsumer implements Consumer {
         @Cleanup(value = "disconnect")
         HttpsURLConnection connector = connectionService.connect(urlParameter);
 
+        // Even when the server returns an error, the status code is 200, but the Content-Type is text/html.
+        if (!"application/json".equals(connector.getHeaderField("Content-Type"))) {
+            log.log(Level.INFO, "ERROR. Error receiving the content from " + urlParameter);
+            return null; //TODO: return a error object.
+        }
+
         StringBuilder response = new StringBuilder();
 
-        try (BufferedReader rd = new BufferedReader(new InputStreamReader(connector.getInputStream()))) {
+        try (BufferedReader rd = new BufferedReader(new InputStreamReader(connector.getInputStream(), StandardCharsets.UTF_8))) {
             String line;
             while ((line = rd.readLine()) != null) {
                 response.append(line);
